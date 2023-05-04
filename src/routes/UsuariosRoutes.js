11dfd3fd51e52a68c51ce63d8c5b98276cd9c321
joao-pedro01 <joao_pedro01@terra.com.br>
2023-05-04 17:58:20 -0300

    Atualizado a swagger.json, rota do usuario adicionado metodo post e get

--- src/routes/UsuariosRoutes.js
+++ src/routes/UsuariosRoutes.js
@@ -5,8 +5,9 @@ const router = express.Router();
 
 // rotas para acoes usuarios
 router
-    .post("/api/logout", UsuarioController.logout)
-    .get("/api/usuarios", UsuarioController.listarUsuarios)
-    .post("/api/cadastro", UsuarioController.cadastrarUsuario)
-    .post("/api/entrar", UsuarioController.loginUsuario)
+.post("/api/usuarios/logout", UsuarioController.logout)
+.get("/api/usuarios/", UsuarioController.listarUsuarios)
+.post("/api/usuarios/cadastro", UsuarioController.cadastrarUsuario)
+.post("/api/usuarios/entrar", UsuarioController.loginUsuario)
+
 export default router;
